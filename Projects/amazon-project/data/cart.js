45ab901459006e6a7a7638eb7be9a4350c3b2bef
pinklamb <chanellelegrand59@gmail.com>
2025-08-14 20:11:50 -0400

    Save Data and generate HTML for delivery options

--- Projects/amazon-project/data/cart.js
+++ Projects/amazon-project/data/cart.js
@@ -1,16 +1,31 @@
 import { products } from "./products.js";
 
 
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-  },
 
-  {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-  }
-];
+export let cart = 
+JSON.parse(localStorage.getItem('cart'));
+
+// gives default value if no item in storage 
+if (!cart){
+    cart = [{ 
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+    deliveryOptionsId: '1'
+    },
+
+    {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+    deliveryOptionsId:'2'
+    }
+  ];
+}
+
+//saves cart so if refreshed it is not deleted
+function savetoStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 
 //add to cart 
 export function addToCart(productId){
@@ -26,9 +41,12 @@ export function addToCart(productId){
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionsId:'1'
       });
     }
+
+    savetoStorage();
 }
 
 //remove from cart
@@ -42,4 +60,6 @@ export function removeCartItem(productId){
     }
   });
   cart = newCart;
+
+  savetoStorage();
 }
---
+++ Projects/amazon-project/data/deliveryOptions.js
@@ -0,0 +1,15 @@
+export const deliveryOptions = [{
+  id:'1',
+  shippingDays:'7',
+  priceCents: 0
+},
+{
+  id:'2',
+  shippingDays:'3',
+  priceCents: 499
+},
+{
+  id:'3',
+  shippingDays:'1',
+  priceCents: 999
+}];
