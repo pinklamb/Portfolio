228bc4fe94ec053f5b5414afd28514e77361a1b5
pinklamb <chanellelegrand59@gmail.com>
2025-08-19 14:17:28 -0400

    Created Automated Testing for addtoCart

--- Projects/amazon-project/data/cart.js
+++ Projects/amazon-project/data/cart.js
@@ -1,23 +1,26 @@
+export let cart;
+
+loadFromStorage();
 
 
-export let cart = 
+export function loadFromStorage () {
+ cart = 
  JSON.parse(localStorage.getItem('cart'));
 
 // gives default value if no item in storage 
-if (!cart){
-    cart = [{ 
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionsId: '1'
-    },
-
-    {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionsId:'2'
-    }
-  ];
-}
+  if (!cart) {
+      cart = [{ 
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionsId: '1'
+      },
+      {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionsId:'2'
+    }];
+  };
+};
 
 //saves cart so if refreshed it is not deleted
 function savetoStorage(){
